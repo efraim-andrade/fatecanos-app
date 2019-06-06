62b99fa5069946e4861a22c120ba4d1458af6508
Efraim Andrade <efraim.dev@gmail.com>
2019-06-06 20:26:10 -0300

    trazendo user info do firebase.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:community_material_icon/community_material_icon.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../components/Card.dart';
 import '../components/Input.dart';
@@ -9,7 +9,6 @@ import '../components/UserInfo.dart';
 import '../components/CreateBigWallModal.dart';
 
 class HomePage extends StatefulWidget {
-
   HomePage({Key key}) : super(key: key); // prop-types
 
   @override
@@ -21,17 +20,40 @@ class Home extends State<HomePage> {
   TextEditingController courseController = new TextEditingController();
   TextEditingController bigWallDayController = new TextEditingController();
 
+  var name = '';
+  var course = '';
+  var period = '';
+  var avatar = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    final Firestore db = Firestore.instance;
+
+    var userInfo = db.collection('user').document('1310481711001').get();
+
+    print('chegou aqui');
+    userInfo.then((doc) {
+      setState(() {
+        name = doc.data['name'];
+        course = doc.data['course'];
+        period = doc.data['period'];
+        avatar = doc.data['avatar64'];
+      });
+    });
+  }
+
   openBigWallModal() {
     showDialog(
-      context: context,
-      child: createBigWallModal(courseController, bigWallDayController)
-    );
+        context: context,
+        child: createBigWallModal(courseController, bigWallDayController));
   }
 
   var courses = [
     {
       "course": "Laboratorio de Engenharia de Software IV",
-      "teacher": "Danilo Ruy Gomes" ,
+      "teacher": "Danilo Ruy Gomes",
       "presences": 28,
       "absenses": 12,
       "maxAbsences": 20,
@@ -39,7 +61,7 @@ class Home extends State<HomePage> {
     },
     {
       "course": "Programação Web",
-      "teacher": "José Antônio Castanho de Almeida" ,
+      "teacher": "José Antônio Castanho de Almeida",
       "presences": 22,
       "absenses": 18,
       "maxAbsences": 20,
@@ -47,7 +69,7 @@ class Home extends State<HomePage> {
     },
     {
       "course": "Engenharia de Software IV",
-      "teacher": "Danilo Ruy Gomes" ,
+      "teacher": "Danilo Ruy Gomes",
       "presences": 28,
       "absenses": 12,
       "maxAbsences": 20,
@@ -55,7 +77,7 @@ class Home extends State<HomePage> {
     },
     {
       "course": "Programação Web",
-      "teacher": "José Antônio Castanho de Almeida" ,
+      "teacher": "José Antônio Castanho de Almeida",
       "presences": 22,
       "absenses": 18,
       "maxAbsences": 20,
@@ -63,7 +85,7 @@ class Home extends State<HomePage> {
     },
     {
       "course": "Engenharia de Software IV",
-      "teacher": "Danilo Ruy Gomes" ,
+      "teacher": "Danilo Ruy Gomes",
       "presences": 28,
       "absenses": 12,
       "maxAbsences": 20,
@@ -71,7 +93,7 @@ class Home extends State<HomePage> {
     },
     {
       "course": "Programação Web",
-      "teacher": "José Antônio Castanho de Almeida" ,
+      "teacher": "José Antônio Castanho de Almeida",
       "presences": 22,
       "absenses": 18,
       "maxAbsences": 20,
@@ -86,106 +108,111 @@ class Home extends State<HomePage> {
         backgroundColor: Color.fromRGBO(250, 250, 250, 1),
         elevation: 0.0,
         actions: <Widget>[
-            IconButton(
-              color: Colors.black87,
-              icon: Icon(CommunityMaterialIcons.bell, size: 30,),
-              onPressed: () {print("pressed");},
-            )
+          IconButton(
+            color: Colors.black87,
+            icon: Icon(
+              CommunityMaterialIcons.bell,
+              size: 30,
+            ),
+            onPressed: () {
+              print("pressed");
+            },
+          )
         ],
         leading: IconButton(
           color: Colors.deepOrangeAccent,
-          icon: Icon(CommunityMaterialIcons.wall, size: 30,),
-          onPressed: () {openBigWallModal();},
+          icon: Icon(
+            CommunityMaterialIcons.wall,
+            size: 30,
+          ),
+          onPressed: () {
+            openBigWallModal();
+          },
         ),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(250, 250, 250, 1)
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                child: userInfo(
-                  "Efraim de Andrade Morais Junior",
-                  "https://avatars1.githubusercontent.com/u/28229600?s=460&v=4",
-                  "Análise e Desenvolvimento de Sistemas"
+          decoration: BoxDecoration(color: Color.fromRGBO(250, 250, 250, 1)),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: userInfo(name, avatar, course),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                child: Row(
-                  children: <Widget>[
-                    Flexible(
-                      flex: 1,
-                      child: input(searchController, true, false, "Buscar matéria"),
-                    ),
-                    Flexible(
-                      flex: 0,
-                      child: Container(
-                        width: 40,
-                        child: IconButton(
-                          onPressed: () {print("pressed");},
-                          icon: Icon(
-                            FontAwesomeIcons.sync,
-                            size: 14,
-                            color: Colors.deepOrangeAccent,
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: input(
+                            searchController, true, false, "Buscar matéria"),
+                      ),
+                      Flexible(
+                        flex: 0,
+                        child: Container(
+                          width: 40,
+                          child: IconButton(
+                            onPressed: () {
+                              print("pressed");
+                            },
+                            icon: Icon(
+                              FontAwesomeIcons.sync,
+                              size: 14,
+                              color: Colors.deepOrangeAccent,
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
+                Flexible(
+                  flex: 1,
+                  // child: StreamBuilder(
+                  //   stream: Firestore.instance.collection('user').document('1310481711001').snapshots(),
+                  //   builder: (context, snapshot) {
+                  //     if(!snapshot.hasData) return const Text("Loading...");
 
-              Flexible(
-                flex: 1,
-                // child: StreamBuilder(
-                //   stream: Firestore.instance.collection('user').document('1310481711001').snapshots(),
-                //   builder: (context, snapshot) {
-                //     if(!snapshot.hasData) return const Text("Loading...");
-
-                //     return ListView.builder(
-                //       itemCount: snapshot.data.documents.length,
-                //       itemBuilder: (context, index) {
-                //         return Container(
-                //           margin: const EdgeInsets.only(bottom: 8),
-                //           child: card(
-                //             snapshot.data.documents[index]["course"],
-                //             snapshot.data.documents[index]["teacher"],
-                //             snapshot.data.documents[index]["presences"],
-                //             snapshot.data.documents[index]["absenses"],
-                //             snapshot.data.documents[index]["maxAbsences"],
-                //             snapshot.data.documents[index]["grade"],
-                //           ),
-                //         );
-                //       },
-                //     );
-                //   },
-                // ),
-                child: ListView.builder(
-                  itemCount: courses.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 8),
-                      child: card(
-                        courses[index]["course"],
-                        courses[index]["teacher"],
-                        courses[index]["presences"],
-                        courses[index]["absenses"],
-                        courses[index]["maxAbsences"],
-                        courses[index]["grade"],
-                      ),
-                    );
-                  },
+                  //     return ListView.builder(
+                  //       itemCount: snapshot.data.documents.length,
+                  //       itemBuilder: (context, index) {
+                  //         return Container(
+                  //           margin: const EdgeInsets.only(bottom: 8),
+                  //           child: card(
+                  //             snapshot.data.documents[index]["course"],
+                  //             snapshot.data.documents[index]["teacher"],
+                  //             snapshot.data.documents[index]["presences"],
+                  //             snapshot.data.documents[index]["absenses"],
+                  //             snapshot.data.documents[index]["maxAbsences"],
+                  //             snapshot.data.documents[index]["grade"],
+                  //           ),
+                  //         );
+                  //       },
+                  //     );
+                  //   },
+                  // ),
+                  child: ListView.builder(
+                    itemCount: courses.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(bottom: 8),
+                        child: card(
+                          courses[index]["course"],
+                          courses[index]["teacher"],
+                          courses[index]["presences"],
+                          courses[index]["absenses"],
+                          courses[index]["maxAbsences"],
+                          courses[index]["grade"],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-        )
-      ),
+              ],
+            ),
+          )),
     );
   }
 }
